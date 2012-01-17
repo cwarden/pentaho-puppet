a0d709e1a74ff535e0732bdceb394f6ef59f46f2
Christian G. Warden <cwarden@xerus.org>
2012-01-17 08:32:57 -0800

    New pentaho::cst class
    New pentaho::cst class to set up Community Startup Tabs (CST).

---
+++ manifests/cst.pp
@@ -0,0 +1,38 @@
+class pentaho::cst {
+  include pentaho::apt_source
+  include concat::setup
+  package { "pentaho-cst":
+    ensure   => "latest",
+    notify   => Service['bi-server'],
+  }
+
+  pentaho::biserver::acl-entry{ 'CST - Authenticated':
+    role          => 'Authenticated',
+    solution      => 'CST',
+    execute_perm  => true,
+    position      => 1
+  }
+
+  pentaho::biserver::acl-entry{ 'CST - Admin':
+    role          => 'Admin',
+    solution      => 'CST',
+    full_control  => true,
+    position      => 0
+  }
+
+  $cst_config = '/opt/pentaho/biserver-ce/pentaho-solutions/CST/cst-config.xml'
+  concat { $cst_config:
+  }
+  concat::fragment {
+    'cst-config-header':
+      target  => $cst_config,
+      order   => '01',
+      content => '<?xml version="1.0"?><cstConfig>';
+    'cst-config-footer':
+      target  => $cst_config,
+      order   => '99',
+      content => '</cstConfig>';
+  }
+
+}
+
