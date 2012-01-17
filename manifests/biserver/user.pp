a509a8ff0306073ebb071eb7daa22f7d81ef1ff8
Christian G. Warden <cwarden@xerus.org>
2012-01-17 08:36:43 -0800

    whitespace fix

--- manifests/biserver/user.pp
+++ manifests/biserver/user.pp
@@ -16,18 +16,18 @@ define pentaho::biserver::user($username = $title, $password, $authorities = ['A
     INSERT INTO GRANTED_AUTHORITIES (USERNAME, AUTHORITY) VALUES ('${username}', '<%= val -%>');
     <% end %>
     COMMIT;"
-    $sql = inline_template($sql_tmpl)
-    $sql_file = md5($sql)
-    $sql_path = "/opt/pentaho/biserver-ce/data/puppet/create-user-${sql_file}.sql"
+  $sql = inline_template($sql_tmpl)
+  $sql_file = md5($sql)
+  $sql_path = "/opt/pentaho/biserver-ce/data/puppet/create-user-${sql_file}.sql"
 
-    file { "${sql_path}":
-      owner   => 'root',
-      mode    => '600',
-      content => $sql,
-    }
-    exec { "create biserver user $title":
-      command => "mysql -h ${pentaho::config::database_host} -u${pentaho::config::hibernate_user} -p${pentaho::config::hibernate_password} ${pentaho::config::hibernate_database} < ${sql_path} || rm -f $sql_path",
-      refreshonly => true,
-      subscribe => File[$sql_path]
-    }
+  file { "${sql_path}":
+    owner   => 'root',
+    mode    => '600',
+    content => $sql,
+  }
+  exec { "create biserver user $title":
+    command => "mysql -h ${pentaho::config::database_host} -u${pentaho::config::hibernate_user} -p${pentaho::config::hibernate_password} ${pentaho::config::hibernate_database} < ${sql_path} || rm -f $sql_path",
+    refreshonly => true,
+    subscribe => File[$sql_path]
+  }
 }
