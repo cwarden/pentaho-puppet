f2785233abb544a95a02c81e01526a942733ef2e
Christian G. Warden <cwarden@xerus.org>
2012-01-19 19:37:35 -0800

    add TODO: refresh repository

--- manifests/biserver/run.pp
+++ manifests/biserver/run.pp
@@ -35,6 +35,10 @@ class pentaho::biserver::run {
       ]
     ],
   }
+
+# TODO: trigger refresh of repository after solutions change, e.g.
+# curl -q http://localhost:8080/pentaho/Publish?userid=joe&password=password&publish=now&class=org.pentaho.platform.engine.services.solution.SolutionPublisher
+
 }
 
 
