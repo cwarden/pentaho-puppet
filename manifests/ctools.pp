fee0fe40562782ceb0cd7ba552cd3d4000ea530e
Christian G. Warden <cwarden@xerus.org>
2012-01-22 12:28:13 -0800

    Install ruby json package
    The json library is used to contruct URLs for setting up CDA caching.

--- manifests/ctools.pp
+++ manifests/ctools.pp
@@ -1,7 +1,10 @@
 class pentaho::ctools {
   include pentaho::apt_source
-  package { "ctools":
-    ensure   => "latest",
-    notify   => Service['bi-server'],
+  package {
+    'ctools':
+      ensure   => "latest",
+      notify   => Service['bi-server'];
+    'libjson-ruby':
+      ensure => installed;
   }
 }
