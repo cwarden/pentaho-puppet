183f0496b53c54b6e3a0421ec0c36fee50eac624
Christian G. Warden <cwarden@xerus.org>
2012-01-26 18:28:00 -0800

    Use 'biserver' as name
    User $vhost_name only to set server_name since it can include regex,
    wildcards, etc.

--- manifests/biserver/proxy.pp
+++ manifests/biserver/proxy.pp
@@ -6,7 +6,8 @@ class pentaho::biserver::proxy($vhost_name = 'pentaho', $vhost_port = 80) {
       'localhost:8080',
     ],
   }
-  nginx::resource::vhost { "${vhost_name}":
+  nginx::resource::vhost { "biserver":
+    server_name => $vhost_name,
     listen_port => $vhost_port,
     ensure   => present,
     proxy  => 'http://bi-server',
